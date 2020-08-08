73fe92356f6402beacc7542e29017c1be99c88aa
patrickwhardy <patrickwhardy@gmail.com>
2020-08-08 12:11:38 -0600

    add beautiful chat to page

--- src/front-end/index.js
+++ src/front-end/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
+import Chat from 'vue-beautiful-chat'
 import ChatBot from './components/chat-bot.vue';
 
+Vue.use(Chat);
 Vue.component('chat-bot', ChatBot);
 new Vue({
     el: `#chat-bot`
