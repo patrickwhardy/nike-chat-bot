b3cd3ea2960dc27fa28a7094bef8ecb7bc70ea9f
patrickwhardy <patrickwhardy@gmail.com>
2020-08-08 16:12:30 -0600

    build out chat logic

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,37 @@
 import express from 'express';
 import env from './apis/env';
 import model from './apis/model';
+const mockResponse = {
+  probabilities: [
+  {
+  probability: 0.5205156,
+  label: "Shipping Info"
+  },
+  {
+  probability: 0.1776938,
+  label: "Order Change"
+  },
+  {
+  probability: 0.11082061,
+  label: "Billing"
+  },
+  {
+  probability: 0.09835051,
+  label: "Password Help"
+  },
+  {
+  probability: 0.092619486,
+  label: "Sales Opportunity"
+  }
+  ],
+  object: "predictresponse"
+};
 
 const PORT = env.PORT;
 const start = async () => {
   try {
     const app = express();
-    app.get('/answers', async (req, res) => {
+    app.get('/intent', async (req, res) => {
       try {
         const { phrase } = req.query;
         if (phrase === undefined || phrase.trim() === '') {
@@ -19,6 +44,7 @@ const start = async () => {
         console.log(error);
         res.status(500).send();
       }
+      // res.send(mockResponse);
     });
     app.get('/', async (req, res) => {
       return res.sendFile('nike-soccer.html', { root: __dirname });
